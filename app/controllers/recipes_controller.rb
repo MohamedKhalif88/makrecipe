472998f7a14b7996893cbd76c880d925b63f47dc
makfauzan <simg1881@gmail.com>
2017-02-14 16:39:19 +0300

    partials and other fuctionalities

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -10,12 +10,28 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
   end
   def create
-    @recipe = Recipe.create(recipe_params)
+    @recipe = Recipe.new(recipe_params)
+    @recipe.chef =Chef.find(1)
+
     if @recipe.save
       flash[:success]="Your recipe was created successfully!"
-      redirect_to recipes_path(recipe)
+      redirect_to recipes_path
     else
-      render 'new'
+      render :new
+    end
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe =Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      flash[:success]="Your Recipe was updated successfully!"
+      redirect_to recipe_path(@recipe)
+    else
+      render :edit
     end
   end
 
